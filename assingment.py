a3d0e120463f5499fa4ea7dc60d6c25f9ea18794
sevvalakar2 <sevval.akar@std.yeditepe.edu.tr>
2025-11-01 18:03:47 +0300

    Add files via upload

---
+++ assingment.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  1 17:29:50 2025
+
+@author: HP
+"""
+import numpy as np
+
+def find_period(L0, L1):
+    g = 9.81
+    T0 = 0
+    T1 = 0
+    
+    for L in range(L0, L1 + 1):
+        T = 2 * np.pi * np.sqrt(L / g)
+        T_rounded = np.round(T, 1)
+        print(f"When L = {L}.0 m, T = {T_rounded} s")
+        
+        if L == L0:
+            T0 = T
+        if L == L1:
+            T1 = T
+    
+    return T0, T1
+
